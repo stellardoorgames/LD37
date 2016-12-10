838d2eb08efc20034a58e3a5a570d1b33e67d7dc
adsilcott <adsilcott@gmail.com>
2016-12-10 16:44:59 -0500

    Adding beginning of level

---
+++ Assets/_Project/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+	public Transform target;
+
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+
+		agent.destination = target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
