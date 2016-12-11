8e3ff415aa75623db946ab79ecd3465b70899b23
adsilcott <adsilcott@gmail.com>
2016-12-10 21:33:22 -0500

    Tenticles

---
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+
+
+		transform.Translate (horizontal * speed, 0f, vertical * speed);
+	}
+}
---
+++ Assets/_Project/Scripts/TenticleController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FluffyUnderware.Curvy;
+using FluffyUnderware.Curvy.Generator;
+
+public class TenticleCell
+{
+	public int index;
+	CurvySplineSegment segment;
+	CurvySpline spline;
+
+	public TenticleCell(int index, CurvySplineSegment segment, CurvySpline spline)
+	{
+		this.index = index;
+		this.segment = segment;
+		this.spline = spline;
+
+	}
+}
+
+public class TenticleController : MonoBehaviour {
+
+	public Transform target;
+
+	public CurvySpline spline;
+
+	public float segmentLength = 2f;
+
+	public int gridWidth = 20;
+
+	public Stack<TenticleCell> cellStack;
+
+	void Start()
+	{
+		spline.Add ();
+
+		cellStack = new Stack<TenticleCell> ();
+		foreach (CurvySplineSegment s in spline.Segments)
+			cellStack.Push (new TenticleCell (1, s, spline));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		CurvySplineSegment segment = spline.LastVisibleControlPoint;
+		//CurvySplineSegment lastSegment = spline.Segments[spline.Count];
+
+		segment.transform.position = target.position;
+
+
+		//if ()
+
+		//Debug.Log (segment.PreviousControlPoint.Length);
+
+		if (segment.PreviousControlPoint.Length > segmentLength)
+		{
+			 
+			segment = spline.Add ();
+			segment.transform.position = target.position;
+		}
+
+		/*if (Input.GetKeyDown (KeyCode.P))
+		{
+			
+			spline.ControlPoints [5].SplitSpline ();
+			//segment = spline.ControlPoints[spline.ControlPointCount - 1];
+			//transform.position = segment.transform.position;
+		}*/
+
+	}
+
+	/*int GetIndexFromPosition(Vector3 pos)
+	{
+		//return (pos);
+	}*/
+}
