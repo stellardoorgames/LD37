89f9b0c60b8e7af6ac377c6b431e689875b73da0
islane <islane@rocketmail.com>
2016-12-11 22:24:20 -0500

    Procedural level generation scripts

---
+++ Assets/_Project/Scripts/Testing/CellRelativePos.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellRelativePos : MonoBehaviour
+{
+
+    #region Attributes
+    public Cell cell;
+    public Direction direction;
+    #endregion
+
+    #region Enums
+    public enum Direction
+    {
+        North,
+        South,
+        East,
+        West
+    }
+    #endregion
+
+    #region Constructor
+    public CellRelativePos(Cell cell, Direction direction)
+    {
+        this.cell = cell;
+        this.direction = direction;
+    }
+    #endregion
+}
