10d458caaa697a896c78a50ebf662e26938c5e25
islane <islane@rocketmail.com>
2016-12-12 21:07:33 -0500

    Title scene, credits scene, scene controller

---
+++ Assets/_Project/Scripts/SceneController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    public void OnPlay()
+    {
+        StartCoroutine(ChangeScene("IntroScene01"));
+    }
+
+    public void OnCredits()
+    {
+        StartCoroutine(ChangeScene("CreditsScene01"));
+    }
+
+    public void OnBack()
+    {
+        StartCoroutine(ChangeScene("MenuScene01"));
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator ChangeScene(string scene)
+    {
+
+        yield return null;
+        SceneManager.LoadScene(scene);
+    }
+
+
+}
