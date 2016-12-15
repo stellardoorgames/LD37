c4fdaeee1886dbacedc0c6d53834d8ad2c9e4d28
adsilcott <adsilcott@gmail.com>
2016-12-14 23:52:03 -0500

    no message

--- Assets/_Project/Scripts/Characters/WizardController.cs
+++ Assets/_Project/Scripts/Characters/WizardController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class WizardController : EnemyController {
 
+	public float attackInterval = 1;
+
+	bool isAttacking;
 
 	protected override void OnTriggerEnter (Collider other)
 	{
@@ -13,10 +16,46 @@ public class WizardController : EnemyController {
 		{
 			Debug.Log ("Attack");
 			
-			agent.Stop ();
-			anim.SetBool ("isAttacking", true);
-			//Start attack
+
+
+			StartCoroutine (AttackCoroutine ());
 		}
 
+	}
+
+	public override void Update ()
+	{
+		base.Update ();
+
+		Collider[] colliders = Physics.OverlapSphere (transform.position, 1f);
+		bool isTentacle = false;
+		foreach (Collider c in colliders)
+			if (c.tag == "Tentacle")
+				isTentacle = true;
+		isAttacking = isTentacle;
+	}
+
+	IEnumerator AttackCoroutine()
+	{
+		isAttacking = true;
+
+		anim.SetBool ("isAttacking", true);
+
+		agent.Stop ();
+
+		while (isAttacking)
+		{
+			anim.SetTrigger ("Attack");
+			
+			float nextAttackTime = Time.time + attackInterval;
+			
+			while (Time.time < nextAttackTime)
+				yield return null;
+			
+		}
+
+		agent.Resume ();
+
+		anim.SetBool ("isAttacking", false);
 	}
 }
